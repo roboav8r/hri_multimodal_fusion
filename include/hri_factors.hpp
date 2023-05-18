eb91b10037398c12e453b2a6c2522bee7e1244a8
JD <john.a.duncan@utexas.edu>
2023-05-18 12:24:52 -0500

    Add initial oakd camera calibration factor

--- include/hri_factors.hpp
+++ include/hri_factors.hpp
@@ -39,6 +39,54 @@ public:
             gtsam::NonlinearFactor::shared_ptr(new OakDInferenceFactor(*this)));}
 };
 
+// Measurement factors
+// See https://github.com/borglab/gtsam/blob/develop/examples/LocalizationExample.cpp
+class OakDCalibrationFactor: public gtsam::NoiseModelFactor2<gtsam::Vector6, gtsam::noiseModel::Diagonal> {
+    double mx_,my_,mz_; // x,y,z measurements
+    gtsam::SharedNoiseModel zero_noise_  =
+      gtsam::noiseModel::Constrained::Sigmas(gtsam::Vector6(0,0,0,0,0,0));
+
+public:
+
+    using gtsam::NoiseModelFactor2<gtsam::Vector6, gtsam::noiseModel::Diagonal>::evaluateError;
+
+    typedef std::shared_ptr<OakDCalibrationFactor> shared_ptr;
+
+    OakDCalibrationFactor(gtsam::Key j, gtsam::Key k, double x, double y, double z):
+        gtsam::NoiseModelFactor2<gtsam::Vector6, gtsam::noiseModel::Diagonal>(zero_noise_, j, k), mx_(x), my_(y), mz_(z) {}
+
+    ~OakDCalibrationFactor() override {}
+
+    gtsam::Vector evaluateError (const gtsam::Vector6& X, const gtsam::noiseModel::Diagonal& Sigma, gtsam::OptionalMatrixType H1, gtsam::OptionalMatrixType H2 ) const override
+    {
+        if (H1) (*H1) = (gtsam::Matrix(6,6) << (X(0) - mx_)/pow(Sigma.sigma(0),2), 0, 0, 0, 0, 0, 
+                                             0, (X(1) - my_)/pow(Sigma.sigma(1),2), 0, 0, 0, 0,
+                                             0, 0, (X(2) - mz_)/pow(Sigma.sigma(2),2), 0, 0, 0,
+                                             0, 0, 0, 0, 0, 0,
+                                             0, 0, 0, 0, 0, 0,
+                                             0, 0, 0, 0, 0, 0).finished(); // Oak-D should directly measure position in camera frame
+        if (H2) (*H2) = (gtsam::Matrix(6,6) << -pow((X(0) - mx_),2)/pow(Sigma.sigma(0),3), 0, 0, 0, 0, 0, 
+                                               0, -pow((X(1) - mx_),2)/pow(Sigma.sigma(1),3), 0, 0, 0, 0,
+                                               0, 0, -pow((X(2) - mx_),2)/pow(Sigma.sigma(2),3), 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0).finished(); // Oak-D should directly measure position in camera frame
+
+        return (gtsam::Vector(6) << 0.5*pow((X(0) - mx_),2)/pow(Sigma.sigma(0),2), 
+                                    0.5*pow((X(1) - my_),2)/pow(Sigma.sigma(1),2), 
+                                    0.5*pow((X(2) - mz_),2)/pow(Sigma.sigma(2),2),
+                                    0,
+                                    0,
+                                    0).finished();
+    }
+
+    gtsam::NonlinearFactor::shared_ptr clone() const override {
+        return std::static_pointer_cast<gtsam::NonlinearFactor>(
+            gtsam::NonlinearFactor::shared_ptr(new OakDCalibrationFactor(*this)));}
+};
+
+
+
 
 /**
  * Constraint to enforce dynamics between the velocities and poses, using
